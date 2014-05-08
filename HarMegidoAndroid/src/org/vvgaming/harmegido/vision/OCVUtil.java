c8b8c6a541d8ab18b9479e0909b51382a7cf19f7
Vinicius Nogueira <viniciusln@gmail.com>
2014-05-08 11:47:45 -0300

    Ajuste na classe de utilitários

--- HarMegidoAndroid/src/org/vvgaming/harmegido/vision/OCVUtil.java
+++ HarMegidoAndroid/src/org/vvgaming/harmegido/vision/OCVUtil.java
@@ -6,18 +6,32 @@ import org.opencv.core.Mat;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.Config;
 
+/**
+ * Classe de utilitários para interação com Objetos da OpenCV
+ * 
+ * @author Vinicius Nogueira
+ */
 public class OCVUtil {
 
-	public static void releaseMat(Mat mat) {
-		if (mat != null)
-			mat.release();
-	}
-
+	/**
+	 * Libera as matrizes, se não nulas
+	 * 
+	 * @param mats
+	 */
 	public static void releaseMat(Mat... mats) {
-		for (Mat m : mats)
-			releaseMat(m);
+		for (Mat m : mats) {
+			if (m != null) {
+				m.release();
+			}
+		}
 	}
 
+	/**
+	 * Converte Mat da OpenCV em Bitmap do Android
+	 * 
+	 * @param mat
+	 * @return o Bitmap
+	 */
 	public static Bitmap toBmp(Mat mat) {
 		Bitmap retorno = Bitmap.createBitmap(mat.cols(), mat.rows(),
 				Config.RGB_565);

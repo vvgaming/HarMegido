df9370a4667d742a9e808714b13d33155c700a7d
Vinicius Nogueira <viniciusln@gmail.com>
2014-05-23 22:00:42 -0300

    novo recurso

--- HarMegidoAndroid/src/org/vvgaming/harmegido/gameEngine/geometry/Retangulo.java
+++ HarMegidoAndroid/src/org/vvgaming/harmegido/gameEngine/geometry/Retangulo.java
@@ -19,12 +19,13 @@ public class Retangulo {
 				(int) (origem.y + h));
 	}
 
-	public boolean contem(Ponto ponto){
-		return (ponto.x >= origem.x && 
-				ponto.x <= (origem.x + w) &&	
-				ponto.y >= origem.y && 
-				ponto.y <= (origem.y + h));
-		
+	public boolean contem(Ponto ponto) {
+		return (ponto.x >= origem.x && ponto.x <= (origem.x + w)
+				&& ponto.y >= origem.y && ponto.y <= (origem.y + h));
+	}
+
+	public Ponto getCenter() {
+		return new Ponto(origem.x + w / 2, origem.y + h / 2);
 	}
 
 }

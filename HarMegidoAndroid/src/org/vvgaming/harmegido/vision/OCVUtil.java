06aab041efb57c6441dc2a6263c384a0980d4404
Vinicius Nogueira <viniciusln@gmail.com>
2014-05-24 00:30:25 -0300

    - Mudança para singleton, para fazer inicialização assincrona - Adição do método de cálculo de histograma em HSV (somente o H e S)

--- HarMegidoAndroid/src/org/vvgaming/harmegido/vision/OCVUtil.java
+++ HarMegidoAndroid/src/org/vvgaming/harmegido/vision/OCVUtil.java
@@ -1,7 +1,13 @@
 package org.vvgaming.harmegido.vision;
 
+import java.util.Arrays;
+
 import org.opencv.android.Utils;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfFloat;
+import org.opencv.core.MatOfInt;
+import org.opencv.imgproc.Imgproc;
 
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.Config;
@@ -13,12 +19,30 @@ import android.graphics.Bitmap.Config;
  */
 public class OCVUtil {
 
+	private Mat empty = new Mat();
+
+	// parâmetros para o cálculo de histograma em HSV (ignorando o V)
+	private MatOfInt channels;
+	private MatOfInt histSize;
+	private MatOfFloat ranges;
+
+	private OCVUtil() {
+		// parâmetros para o cálculo de histograma em HSV (ignorando o V)
+		channels = new MatOfInt(0, 1);
+		histSize = new MatOfInt(50, 60);
+		ranges = new MatOfFloat();
+		ranges.put(0, 0, 0.0f);
+		ranges.put(0, 1, 256.0f);
+		ranges.put(1, 0, 0.0f);
+		ranges.put(1, 1, 180.0f);
+	}
+
 	/**
 	 * Libera as matrizes, se não nulas
 	 * 
 	 * @param mats
 	 */
-	public static void releaseMat(Mat... mats) {
+	public void releaseMat(Mat... mats) {
 		for (Mat m : mats) {
 			if (m != null) {
 				m.release();
@@ -29,14 +53,57 @@ public class OCVUtil {
 	/**
 	 * Converte Mat da OpenCV em Bitmap do Android
 	 * 
-	 * @param mat
+	 * @param toConvert
 	 * @return o Bitmap
 	 */
-	public static Bitmap toBmp(Mat mat) {
-		Bitmap retorno = Bitmap.createBitmap(mat.cols(), mat.rows(),
-				Config.RGB_565);
-		Utils.matToBitmap(mat, retorno);
+	public Bitmap toBmp(Mat mat) {
+
+		Mat toConvert = mat.clone();
+
+		Bitmap retorno = Bitmap.createBitmap(toConvert.cols(),
+				toConvert.rows(), Config.RGB_565);
+		Utils.matToBitmap(toConvert, retorno);
+
+		releaseMat(toConvert);
+
 		return retorno;
+	}
+
+	/**
+	 * Cálcula o histograma de H e S de uma imagem RGBA. Isto é, converte a
+	 * imagem para HSV, ignora o V e cácula o histograma
+	 * 
+	 * @param mat
+	 *            a imagem em RGBA
+	 * @return o histograma
+	 */
+	public Mat calcHistHS(final Mat mat) {
+
+		Mat retorno = new Mat();
+		Mat imagem = mat.clone();
+
+		// converte para hsv
+		Imgproc.cvtColor(imagem, imagem, Imgproc.COLOR_RGBA2RGB);
+		Imgproc.cvtColor(imagem, imagem, Imgproc.COLOR_RGB2HSV);
+
+		// cálcula o histograma apenas de H e S
+		Imgproc.calcHist(Arrays.asList(imagem), channels, empty, retorno,
+				histSize, ranges);
+
+		// normalizando o histograma para comparar grandezas de mesmo range
+		Core.normalize(retorno, retorno, 0, 1, Core.NORM_MINMAX, -1, empty);
+		releaseMat(imagem);
+
+		return retorno;
+	}
+
+	private static OCVUtil instance;
+
+	public static OCVUtil getInstance() {
+		if (instance == null) {
+			instance = new OCVUtil();
+		}
+		return instance;
 	}
 
 }

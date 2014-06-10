66e3c783462b0645888f262975c34db84d0d8c53
Vinicius Nogueira <viniciusln@gmail.com>
2014-06-10 16:22:07 -0300

    Inicialização um pouco mais limpa do uOS

--- HarMegidoAndroid/src/org/vvgaming/harmegido/test/Main.java
+++ HarMegidoAndroid/src/org/vvgaming/harmegido/test/Main.java
@@ -1,8 +1,7 @@
 package org.vvgaming.harmegido.test;
 
-import java.util.ListResourceBundle;
-
 import org.unbiquitous.uos.core.ClassLoaderUtils;
+import org.unbiquitous.uos.core.InitialProperties;
 import org.unbiquitous.uos.core.UOS;
 import org.unbiquitous.uos.network.socket.connectionManager.TCPConnectionManager;
 import org.unbiquitous.uos.network.socket.radar.MulticastRadar;
@@ -20,7 +19,7 @@ import android.view.WindowManager;
 import android.widget.Button;
 
 public class Main extends Activity {
-	
+
 	private UOS uos = null;
 
 	@Override
@@ -29,12 +28,12 @@ public class Main extends Activity {
 
 		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 		setContentView(R.layout.main);
-		
-		ClassLoaderUtils.builder = new ClassLoaderUtils.DefaultClassLoaderBuilder(){
-		      public ClassLoader getParentClassLoader() {
-		        return getClassLoader();
-		      };
-		    };
+
+		ClassLoaderUtils.builder = new ClassLoaderUtils.DefaultClassLoaderBuilder() {
+			public ClassLoader getParentClassLoader() {
+				return getClassLoader();
+			};
+		};
 
 		((Button) findViewById(R.id.btnHarMegido))
 				.setOnClickListener(new View.OnClickListener() {
@@ -68,38 +67,36 @@ public class Main extends Activity {
 
 		startUos();
 	}
-	
+
 	private void startUos() {
-	    if (uos != null)
-	      return;
-	    uos = new UOS();
-	    new AsyncTask<Void, Void, Void> () {
-	      protected Void doInBackground(Void... params) {
-	        uos.start(new ListResourceBundle() {
-	          protected Object[][] getContents() {
-	            return new Object[][] {
-	              {"ubiquitos.connectionManager", TCPConnectionManager.class.getName()},
-	              {"ubiquitos.radar", MulticastRadar.class.getName()},
-	              {"ubiquitos.eth.tcp.port", "14984"},
-	              {"ubiquitos.eth.tcp.passivePortRange", "14985-15000"},
-	              {"ubiquitos.eth.udp.port", "15001"},
-	              {"ubiquitos.eth.udp.passivePortRange", "15002-15017"},
-	            };
-	          }
-	        });
-	        return null;
-	      }
-	    }.execute();
-	  }
-	  
-	  private void stopUos() {
-	    if (uos == null)
-	      return;
-	    uos.stop();
-	    uos = null;
-	  }
-	  
-	  @Override
+		if (uos != null)
+			return;
+		uos = new UOS();
+		new AsyncTask<Void, Void, Void>() {
+			protected Void doInBackground(Void... params) {
+
+				InitialProperties props = new InitialProperties();
+
+				props.put("ubiquitos.radar", MulticastRadar.class.getName());
+				props.put("ubiquitos.connectionManager",
+						TCPConnectionManager.class.getName());
+				props.put("ubiquitos.eth.tcp.port", "14984");
+
+				uos.start(props);
+
+				return null;
+			}
+		}.execute();
+	}
+
+	private void stopUos() {
+		if (uos == null)
+			return;
+		uos.stop();
+		uos = null;
+	}
+
+	@Override
 	protected void onDestroy() {
 		super.onDestroy();
 		stopUos();

f562bb898c5d974491a9848687caf636f6eb6492
Vinicius Nogueira <viniciusln@gmail.com>
2014-06-17 01:24:49 -0300

    Implementação do AssetManager, ainda em melhoria

---
+++ HarMegidoAndroid/src/org/vvgaming/harmegido/gameEngine/AssetManager.java
@@ -0,0 +1,45 @@
+package org.vvgaming.harmegido.gameEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+public class AssetManager {
+
+	private final Activity act;
+	private final Map<Integer, Bitmap> bitmaps = new HashMap<>();
+
+	public AssetManager(final Activity act) {
+		this.act = act;
+	}
+
+	public Bitmap getBitmap(final int bmpId) {
+		Bitmap retorno;
+		if ((retorno = bitmaps.get(bmpId)) == null) {
+			bitmaps.put(
+					bmpId,
+					retorno = BitmapFactory.decodeResource(act.getResources(),
+							bmpId));
+		}
+		return retorno;
+	}
+
+	public void release() {
+		for (Entry<Integer, Bitmap> entry : bitmaps.entrySet()) {
+			entry.getValue().recycle();
+		}
+	}
+
+	public android.content.res.AssetManager getAndroidAssets() {
+		return act.getAssets();
+	}
+
+	public Activity getActivity() {
+		return act;
+	}
+
+}

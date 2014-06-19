47ce3a6226ec79ae4f2192d5025b7df86e7edec4
Vinicius Nogueira <viniciusln@gmail.com>
2014-06-19 06:51:28 -0300

    Colocando um texto mais elaborado...

--- HarMegidoAndroid/src/org/vvgaming/harmegido/theGame/mainNodes/N1Intro.java
+++ HarMegidoAndroid/src/org/vvgaming/harmegido/theGame/mainNodes/N1Intro.java
@@ -3,6 +3,8 @@ package org.vvgaming.harmegido.theGame.mainNodes;
 import org.vvgaming.harmegido.gameEngine.GameNode;
 import org.vvgaming.harmegido.gameEngine.RootNode;
 import org.vvgaming.harmegido.gameEngine.nodes.NText;
+import org.vvgaming.harmegido.gameEngine.nodes.NText.VerticalAlign;
+import org.vvgaming.harmegido.gameEngine.nodes.NTextBlinking;
 
 import android.graphics.Paint.Align;
 import android.graphics.Typeface;
@@ -10,23 +12,44 @@ import android.view.MotionEvent;
 
 public class N1Intro extends GameNode {
 
-	private NText orientacao;
-
 	@Override
 	public void init() {
 		super.init();
 
-		final String texto = "Vamos preparar o ambiente para você.\n"
-				+ "Enquanto isso, escolha suas preferências\n\n\n"
-				+ "Toque na tela para continuar...";
-		orientacao = new NText(getGameWidth() / 2, getGameHeight() / 2, texto);
-		orientacao.face = Typeface.createFromAsset(getGameAssetManager()
+		final String texto = "Eles então se congregaram num lugar\n"
+				+ "que em hebreu chama-se Har Megido.\nApocalise 16:13-16\n\n\n"
+				+ "A batalha entre o bem \n" + "e o mal está se espalhando \n"
+				+ "pelo planeta, e é hora de\n "
+				+ "você escolher por qual lado lutar";
+		NText orientacao = new NText((int) getGameWidth(.5f),
+				(int) getGameHeight(.5f), texto);
+		final Typeface font = Typeface.createFromAsset(getGameAssetManager()
 				.getAndroidAssets(), "fonts/Radio Trust.ttf");
+		orientacao.face = font;
 		orientacao.paint.setTextAlign(Align.CENTER);
 		orientacao.vAlign = NText.VerticalAlign.MIDDLE;
 		orientacao.size = 50;
 
+		String toqParaComecarStr = "Toque na tela para começar...";
+		NTextBlinking toqueParaComeçar = new NTextBlinking(
+				(int) getGameWidth(.5f), (int) getGameHeight(.9f),
+				toqParaComecarStr);
+		toqueParaComeçar.face = font;
+		toqueParaComeçar.paint.setTextAlign(Align.CENTER);
+		toqueParaComeçar.vAlign = NText.VerticalAlign.MIDDLE;
+		toqueParaComeçar.size = 60;
+
+		NText title = new NText((int) getGameWidth(.5f),
+				(int) getGameHeight(.1f), "Har Megido");
+		title.face = Typeface.createFromAsset(getGameAssetManager()
+				.getAndroidAssets(), "fonts/dc_o.ttf");
+		title.paint.setTextAlign(Align.CENTER);
+		title.size = 140;
+		title.vAlign = VerticalAlign.TOP;
+
 		addSubNode(orientacao);
+		addSubNode(toqueParaComeçar);
+		addSubNode(title);
 
 	}
 

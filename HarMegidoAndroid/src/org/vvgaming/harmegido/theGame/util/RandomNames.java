ff8f373f978161b6e8e973ff6916f4a66395ae9b
Vinicius Nogueira <viniciusln@gmail.com>
2014-06-21 04:50:00 -0300

    gerador de nomes aleatórios de anjos e demonios

---
+++ HarMegidoAndroid/src/org/vvgaming/harmegido/theGame/util/RandomNames.java
@@ -0,0 +1,44 @@
+package org.vvgaming.harmegido.theGame.util;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+import org.vvgaming.harmegido.R;
+import org.vvgaming.harmegido.gameEngine.util.AssetManager;
+
+public class RandomNames
+{
+
+	private final List<String> angels;
+	private final List<String> demons;
+	private final Random rnd;
+
+	public RandomNames(final AssetManager assetManager)
+	{
+		super();
+		rnd = new Random();
+		angels = Arrays.asList(assetManager.getRawTextFile(R.raw.angels).split("\n"));
+		demons = Arrays.asList(assetManager.getRawTextFile(R.raw.demons).split("\n"));
+		if (angels.isEmpty() || demons.isEmpty())
+		{
+			throw new IllegalArgumentException("arquivo dos nomes de anjos e/ou demonios está vazio");
+		}
+	}
+
+	public String getRandomAngelName()
+	{
+		return angels.get(rnd.nextInt(angels.size()));
+	}
+
+	public String getRandomDemonName()
+	{
+		return demons.get(rnd.nextInt(demons.size()));
+	}
+
+	public String getRandomMatchName()
+	{
+		return getRandomDemonName() + " vs " + getRandomAngelName();
+	}
+
+}

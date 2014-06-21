61fb55c3b1188a33e785ef2e956f993d1a5bb7be
Vinicius Nogueira <viniciusln@gmail.com>
2014-06-21 04:48:46 -0300

    ajustes no texto

--- HarMegidoAndroid/src/org/vvgaming/harmegido/gameEngine/nodes/NText.java
+++ HarMegidoAndroid/src/org/vvgaming/harmegido/gameEngine/nodes/NText.java
@@ -12,10 +12,12 @@ import android.graphics.Typeface;
  * 
  * @author Vinicius Nogueira
  */
-public class NText extends GameNode {
+public class NText extends GameNode
+{
 
-	public enum VerticalAlign {
-		TOP, MIDDLE;
+	public enum VerticalAlign
+	{
+		TOP, BOTTOM, MIDDLE;
 	}
 
 	public Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
@@ -23,76 +25,80 @@ public class NText extends GameNode {
 	public String text;
 	public Ponto pos;
 	public VerticalAlign vAlign = VerticalAlign.MIDDLE;
-	public int size = 12;
+	public Integer size;
 
 	private boolean visible = true;
 
-	public NText() {
+	public NText()
+	{
 		this(0, 0, "");
 	}
 
-	public NText(int x, int y, String text) {
+	public NText(float x, float y, String text)
+	{
 		paint.setARGB(255, 255, 255, 255);
 		pos = new Ponto(x, y);
-		paint.setTextSize(size);
-
 		paint.setTypeface(face);
 		this.text = text;
 	}
 
 	@Override
-	public void update(long delta) {
+	public void init()
+	{
+		if(size == null)
+		size = getSmallFontSize();
+	}
+
+	@Override
+	public void update(long delta)
+	{
 		paint.setTextSize(size);
 		paint.setTypeface(face);
 	}
 
 	@Override
-	public void render(Canvas canvas) {
+	public void render(Canvas canvas)
+	{
 		final String[] splitByLine = text.split("\n");
 		float yStart = pos.y;
-		switch (vAlign) {
+		switch (vAlign)
+		{
 		case TOP:
+			yStart = pos.y + (splitByLine.length * size);
+			break;
+		case BOTTOM:
 			yStart = pos.y;
 			break;
 		case MIDDLE:
 			yStart = pos.y - ((splitByLine.length * size) / 2);
 			break;
 		default:
-			throw new IllegalArgumentException(
-					"Alinhamento vertical desconhecido: " + vAlign);
+			throw new IllegalArgumentException("Alinhamento vertical desconhecido: " + vAlign);
 		}
-		for (int i = 0; i < splitByLine.length; i++) {
+		for (int i = 0; i < splitByLine.length; i++)
+		{
 			canvas.drawText(splitByLine[i], pos.x, yStart + (i * size), paint);
 		}
 	}
 
 	@Override
-	public boolean isDead() {
-		return false;
-	}
-
-	@Override
-	public void init() {
-	}
-
-	@Override
-	public void end() {
-	}
-
-	@Override
-	public boolean isVisible() {
+	public boolean isVisible()
+	{
 		return visible;
 	}
 
-	public void setVisible(boolean visible) {
+	public void setVisible(boolean visible)
+	{
 		this.visible = visible;
 	}
 
-	public void setARGB(final int a, final int r, final int g, final int b) {
+	public void setARGB(final int a, final int r, final int g, final int b)
+	{
 		paint.setARGB(a, r, g, b);
 	}
 
-	public NText clone() {
+	public NText clone()
+	{
 		NText retorno = new NText();
 		retorno.face = face;
 		retorno.size = size;

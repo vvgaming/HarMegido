8a39acaf48b669666bd403cc5f143c40f9c5efbc
detentor <vinicius.seufitele@gmail.com>
2014-06-21 01:20:23 -0300

    Criação do driver do cliente e do método para executar alterações.

---
+++ HarMegidoAndroid/src/org/vvgaming/harmegido/util/MatchManager.java
@@ -0,0 +1,45 @@
+package org.vvgaming.harmegido.util;
+
+import org.vvgaming.harmegido.lib.model.Match;
+
+import com.github.detentor.codex.monads.Option;
+
+/**
+ * Classe criada para controlar a partida ativa (se ela existir). <br/>
+ * Ao invés de manter uma referência direta a uma partida, quem precisar de referência 
+ * a uma partida pode usar apenas essa classe. 
+ */
+public final class MatchManager
+{
+	//A partida atual
+	private static Match partida;
+	
+	private MatchManager()
+	{
+		//singleton
+	}
+	
+	/**
+	 * Defina a partida atual como a partida passada como parâmetro
+	 * @param aPartida A partida a ser definida como partida atual
+	 * @throws IllegalArgumentException Se a partida estiver nula
+	 */
+	public static void definirPartida(final Match aPartida)
+	{
+		if (aPartida == null)
+		{
+			throw new IllegalArgumentException("A partida não pode ser nula");
+		}
+		
+		partida = aPartida; 
+	}
+
+	/**
+	 * Retorna a partida ativa neste momento, se ela existir
+	 * @return Uma {@link Option} que irá conter a partida ativa, se ela existir
+	 */
+	public static Option<Match> getPartida()
+	{
+		return Option.from(partida);
+	}
+}

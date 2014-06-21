b7a7803e46aacd14f551a6356841ecdb18d0227f
Vinicius Nogueira <viniciusln@gmail.com>
2014-06-21 04:37:21 -0300

    incluindo funcionalidade para leitura de arquivos texto

--- HarMegidoAndroid/src/org/vvgaming/harmegido/gameEngine/util/AssetManager.java
+++ HarMegidoAndroid/src/org/vvgaming/harmegido/gameEngine/util/AssetManager.java
@@ -1,5 +1,9 @@
 package org.vvgaming.harmegido.gameEngine.util;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -10,56 +14,112 @@ import android.graphics.BitmapFactory;
 import android.media.MediaPlayer;
 
 /**
- * Gerenciador de recursos que d� acesso aos nativos do Android e mantem alguns
- * em seu dom�nio para evitar duplicidade em mem�ria
+ * Gerenciador de recursos que d� acesso aos nativos do Android e mantem alguns em seu dom�nio para evitar duplicidade em mem�ria
  * 
  * @author Vinicius Nogueira
  */
-public class AssetManager {
+public class AssetManager
+{
 
 	private final Activity act;
 	private final Map<Integer, Bitmap> bitmaps = new HashMap<Integer, Bitmap>();
 	private final Map<Integer, MediaPlayer> medias = new HashMap<Integer, MediaPlayer>();
 
-	public AssetManager(final Activity act) {
+	public AssetManager(final Activity act)
+	{
 		this.act = act;
 	}
 
-	public Bitmap getBitmap(final int bmpId) {
+	public Bitmap getBitmap(final int bmpId)
+	{
 		Bitmap retorno;
-		if ((retorno = bitmaps.get(bmpId)) == null) {
-			bitmaps.put(
-					bmpId,
-					retorno = BitmapFactory.decodeResource(act.getResources(),
-							bmpId));
+		if ((retorno = bitmaps.get(bmpId)) == null)
+		{
+			bitmaps.put(bmpId, retorno = BitmapFactory.decodeResource(act.getResources(), bmpId));
 		}
 		return retorno;
 	}
 
-	public void release() {
-		for (Entry<Integer, Bitmap> entry : bitmaps.entrySet()) {
+	public void release()
+	{
+		for (final Entry<Integer, Bitmap> entry : bitmaps.entrySet())
+		{
 			entry.getValue().recycle();
 		}
 		bitmaps.clear();
-		for (Entry<Integer, MediaPlayer> entry : medias.entrySet()) {
+		for (final Entry<Integer, MediaPlayer> entry : medias.entrySet())
+		{
 			entry.getValue().release();
 		}
 		medias.clear();
 	}
 
-	public void playSound(final int soundId) {
+	public void playSound(final int soundId)
+	{
 		MediaPlayer mp;
-		if ((mp = medias.get(soundId)) == null) {
+		if ((mp = medias.get(soundId)) == null)
+		{
 			medias.put(soundId, mp = MediaPlayer.create(getActivity(), soundId));
 		}
 		mp.start();
 	}
 
-	public android.content.res.AssetManager getAndroidAssets() {
+	public String getRawTextFile(final int rawFileId)
+	{
+		StringBuffer sb = new StringBuffer();
+		InputStream is = null;
+		BufferedReader br = null;
+
+		try
+		{
+			is = act.getResources().openRawResource(rawFileId);
+			br = new BufferedReader(new InputStreamReader(is));
+
+			String line;
+			while ((line = br.readLine()) != null)
+			{
+				sb.append(line + "\n");
+			}
+			return sb.toString();
+		}
+		catch (Exception ignored)
+		{
+			return "";
+		}
+		finally
+		{
+			if (is != null)
+			{
+				try
+				{
+					is.close();
+				}
+				catch (IOException ignored)
+				{
+				}
+			}
+			if (br != null)
+			{
+				try
+				{
+					br.close();
+				}
+				catch (IOException ignored)
+				{
+				}
+			}
+
+		}
+
+	}
+
+	public android.content.res.AssetManager getAndroidAssets()
+	{
 		return act.getAssets();
 	}
 
-	public Activity getActivity() {
+	public Activity getActivity()
+	{
 		return act;
 	}
 

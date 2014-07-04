817ae4e631a25fc888a2f8b46ddb77ace2bf71e5
Vinicius Nogueira <viniciusln@gmail.com>
2014-07-04 11:10:02 -0300

    Ajuste no alinhamento do texto

--- HarMegidoAndroid/src/org/vvgaming/harmegido/test/interfaceTest/NInterfaceCharTest.java
+++ HarMegidoAndroid/src/org/vvgaming/harmegido/test/interfaceTest/NInterfaceCharTest.java
@@ -8,6 +8,7 @@ import java.util.regex.Pattern;
 import org.vvgaming.harmegido.gameEngine.geometry.Ponto;
 import org.vvgaming.harmegido.gameEngine.nodes.NImage;
 import org.vvgaming.harmegido.gameEngine.nodes.NText;
+import org.vvgaming.harmegido.gameEngine.nodes.NText.VerticalAlign;
 import org.vvgaming.harmegido.gameEngine.nodes.buttons.NButtonText;
 import org.vvgaming.harmegido.lib.model.TeamType;
 import org.vvgaming.harmegido.theGame.objNodes.NHMBackground;
@@ -17,6 +18,7 @@ import android.app.Activity;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Paint.Align;
 import android.graphics.Rect;
 
 import com.github.detentor.codex.function.Function0;
@@ -38,85 +40,83 @@ public class NInterfaceCharTest extends NHMMainNode {
 
 	private List<GameChar> angels;
 	private List<GameChar> demons;
-	
+
 	private NImage charImg;
 	private NText nomeChar;
 	private NText flavorText;
-	
 
 	public NInterfaceCharTest() {
 	}
 
 	@Override
 	public void init() {
-		
+
 		super.init();
-		
+
 		angels = loadChars("angel");
 		demons = loadChars("demon");
-		
+
 		tipoTime = TeamType.LIGHT;
-		
-		charImg = new NImage(new Ponto(getGameWidth(.5f), getGameHeight(.40f)), getImage());
+
+		charImg = new NImage(new Ponto(getGameWidth(.5f), getGameHeight(.40f)),
+				getImage());
 		charImg.setWidth(getGameWidth(.65f), true);
-		
+
 		nomeChar = new NText(new Ponto(0, 0), getCurChar().getName());
 		flavorText = new NText(new Ponto(0, 0), getCurChar().getFlavorText());
 
-		atualizarChar(); //atualiza as posições dos botões
+		atualizarChar(); // atualiza as posições dos botões
 
-//		// botão de seleção dos demonios
-		final NButtonText proximoBtn = new NButtonText(new NText(getGameWidth(.10f), getGameHeight(.95f), "próximo"));
-//
+		// // botão de seleção dos demonios
+		final NButtonText proximoBtn = new NButtonText(new NText(
+				getGameWidth(.10f), getGameHeight(.95f), "próximo"));
+		//
 		proximoBtn.setOnClickFunction(new Function0<Void>() {
 			@Override
 			public Void apply() {
-				
-				if (++curPos > 9)
-				{
+
+				if (++curPos > 9) {
 					curPos = 0;
 				}
-				
+
 				atualizarChar();
 				return null;
 			}
 		});
-		
-		final NButtonText trocarTimeBtn = new NButtonText(new NText(getGameWidth(.65f), getGameHeight(.10f), "trocar"));
+
+		final NButtonText trocarTimeBtn = new NButtonText(new NText(
+				getGameWidth(.65f), getGameHeight(.10f), "trocar"));
 		//
 		trocarTimeBtn.setOnClickFunction(new Function0<Void>() {
-					@Override
-					public Void apply() {
-						
-						if (tipoTime == TeamType.DARK)
-						{
-							tipoTime = TeamType.LIGHT;
-							curPos = 0;
-						}
-						else
-						{
-							tipoTime = TeamType.DARK;
-							curPos = 0;
-						}
-						
-						atualizarChar();
-						
-						return null;
-					}
-				});
-		
-//		demoniosBtn.getImage().setWidthKeepingRatio(getGameWidth(.4f));
-//		demoniosTglBtn = new NToggleButton(demoniosBtn);
-//		
-//		// botão de seleção dos anjos
-//		final NButtonImage anjosBtn = new NButtonImage(new NImage(new Ponto(
-//				getGameWidth(.75f), getGameHeight(.4f)), getGameAssetManager()
-//				.getBitmap(R.drawable.kayle)));
-//		anjosBtn.getImage().setWidthKeepingRatio(getGameWidth(.4f));
-//		anjosTglBtn = new NToggleButton(anjosBtn);
+			@Override
+			public Void apply() {
+
+				if (tipoTime == TeamType.DARK) {
+					tipoTime = TeamType.LIGHT;
+					curPos = 0;
+				} else {
+					tipoTime = TeamType.DARK;
+					curPos = 0;
+				}
+
+				atualizarChar();
+
+				return null;
+			}
+		});
+
+		// demoniosBtn.getImage().setWidthKeepingRatio(getGameWidth(.4f));
+		// demoniosTglBtn = new NToggleButton(demoniosBtn);
+		//
+		// // botão de seleção dos anjos
+		// final NButtonImage anjosBtn = new NButtonImage(new NImage(new Ponto(
+		// getGameWidth(.75f), getGameHeight(.4f)), getGameAssetManager()
+		// .getBitmap(R.drawable.kayle)));
+		// anjosBtn.getImage().setWidthKeepingRatio(getGameWidth(.4f));
+		// anjosTglBtn = new NToggleButton(anjosBtn);
 
 		// agrupando os toogles de demonios e anjos
-//		timeTglGroup = new NGroupToggleButton(demoniosTglBtn, anjosTglBtn);
+		// timeTglGroup = new NGroupToggleButton(demoniosTglBtn, anjosTglBtn);
 
 		background = new NHMBackground();
 		addSubNode(background, 0);
@@ -125,98 +125,95 @@ public class NInterfaceCharTest extends NHMMainNode {
 		addSubNode(trocarTimeBtn, 1);
 		addSubNode(flavorText, 1);
 		addSubNode(nomeChar, 1);
-//		addSubNode(timeTglGroup, 1);
-//		addSubNode(orientacao, 1);
+		// addSubNode(timeTglGroup, 1);
+		// addSubNode(orientacao, 1);
 	}
-	
-	private GameChar getCurChar()
-	{
-		return tipoTime == TeamType.LIGHT ? angels.get(curPos) :  demons.get(curPos);
+
+	private GameChar getCurChar() {
+		return tipoTime == TeamType.LIGHT ? angels.get(curPos) : demons
+				.get(curPos);
 	}
-	
-	private void atualizarChar()
-	{
+
+	private void atualizarChar() {
 		final Ponto charPos = charImg.getPos();
-		
-		//Altera as informações do nome do char
+
+		// Altera as informações do nome do char
 		nomeChar.text = getCurChar().getName();
 		nomeChar.setWidth(charImg.getWidth());
-		final Rect nomeCharRect = nomeChar.getTextBounds();
-		nomeChar.pos = new Ponto(charPos.x, charPos.y - (nomeCharRect.height() / 2));
-		
-		//Altera as informações do flavor text
+		nomeChar.vAlign = VerticalAlign.BOTTOM;
+		nomeChar.paint.setTextAlign(Align.LEFT);
+		nomeChar.pos = new Ponto(charPos.x, charPos.y);
+
+		// Altera as informações do flavor text
 		flavorText.text = getCurChar().getFlavorText();
 		flavorText.setWidth(charImg.getWidth());
 		final Rect flavorRect = flavorText.getTextBounds();
-		flavorText.pos = new Ponto(charPos.x, charPos.y + charImg.getHeight() + flavorRect.height());
-		
-		//Troca o bmp do char
+		flavorText.pos = new Ponto(charPos.x, charPos.y + charImg.getHeight()
+				+ flavorRect.height());
+
+		// Troca o bmp do char
 		charImg.setBmp(getImage());
 	}
-	
-	private List<GameChar> loadChars(final String prefix)
-	{
-		final int fileId = idFromName(prefix + "s" , "raw");
-		final List<Tuple2<String, String>> charNames = parseFile(getGameAssetManager().getRawTextFile(fileId));
+
+	private List<GameChar> loadChars(final String prefix) {
+		final int fileId = idFromName(prefix + "s", "raw");
+		final List<Tuple2<String, String>> charNames = parseFile(getGameAssetManager()
+				.getRawTextFile(fileId));
 		final List<GameChar> toReturn = new ArrayList<>();
-		
-		for (int i = 1; i < 11; i++)
-		{
+
+		for (int i = 1; i < 11; i++) {
 			final String fileName = prefix + (i < 10 ? "0" : "") + i;
-			final Tuple2<String, String> curTuple = charNames.get(i-1);
-			toReturn.add(new GameChar(curTuple.getVal1(), curTuple.getVal2(), idFromName(fileName, "drawable")));
+			final Tuple2<String, String> curTuple = charNames.get(i - 1);
+			toReturn.add(new GameChar(curTuple.getVal1(), curTuple.getVal2(),
+					idFromName(fileName, "drawable")));
 		}
-		
+
 		return toReturn;
 	}
-	
-	private List<Tuple2<String, String>> parseFile(final String theFile)
-	{
-		final List<Tuple2<String, String>> toReturn = new ArrayList<>();
-		final Matcher mat = Pattern.compile("([^{]+)\\{([^}]+)\\}").matcher(theFile);
 
-		while (mat.find())
-		{
+	private List<Tuple2<String, String>> parseFile(final String theFile) {
+		final List<Tuple2<String, String>> toReturn = new ArrayList<>();
+		final Matcher mat = Pattern.compile("([^{]+)\\{([^}]+)\\}").matcher(
+				theFile);
+
+		while (mat.find()) {
 			toReturn.add(Tuple2.from(mat.group(1).trim(), mat.group(2).trim()));
 		}
 		return toReturn;
 	}
-	
-	private int idFromName(final String theName, final String theFolder)
-	{
+
+	private int idFromName(final String theName, final String theFolder) {
 		final Activity activity = getGameAssetManager().getActivity();
 		final String packName = activity.getPackageName();
-		return activity.getResources().getIdentifier(theName, theFolder, packName);
+		return activity.getResources().getIdentifier(theName, theFolder,
+				packName);
 	}
-	
-	private Bitmap getImage()
-	{
+
+	private Bitmap getImage() {
 		int imgCode = 0;
-		
-		if (tipoTime == TeamType.DARK)
-		{
+
+		if (tipoTime == TeamType.DARK) {
 			imgCode = demons.get(curPos).getResId();
-		}
-		else
-		{
+		} else {
 			imgCode = angels.get(curPos).getResId();
 		}
-		
-		final Resources theResource = getGameAssetManager().getActivity().getResources();
+
+		final Resources theResource = getGameAssetManager().getActivity()
+				.getResources();
 		return BitmapFactory.decodeResource(theResource, imgCode);
 	}
-	
+
 	@Override
 	public void update(final long delta) {
-//		if (timeTglGroup.getToggledIndex().isEmpty()) {
-//			orientacao.text = "Selecione seu time...";
-//		} else if (timeTglGroup.getToggledIndex().get() == 0) {
-//			orientacao.text = "Demonios...";
-//		} else if (timeTglGroup.getToggledIndex().get() == 1) {
-//			orientacao.text = "Anjos...";
-//		} else {
-//			orientacao.text = "Que isso?";
-//		}
+		// if (timeTglGroup.getToggledIndex().isEmpty()) {
+		// orientacao.text = "Selecione seu time...";
+		// } else if (timeTglGroup.getToggledIndex().get() == 0) {
+		// orientacao.text = "Demonios...";
+		// } else if (timeTglGroup.getToggledIndex().get() == 1) {
+		// orientacao.text = "Anjos...";
+		// } else {
+		// orientacao.text = "Que isso?";
+		// }
 	}
 
 }

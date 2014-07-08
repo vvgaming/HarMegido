668767b6a879f96ac7583261d2acda7eece52ff7
Vinicius Nogueira <viniciusln@gmail.com>
2014-07-08 02:18:35 -0300

    implementação de visibilidade

--- HarMegidoAndroid/src/org/vvgaming/harmegido/theGame/objNodes/NSimpleBox.java
+++ HarMegidoAndroid/src/org/vvgaming/harmegido/theGame/objNodes/NSimpleBox.java
@@ -17,36 +17,39 @@ public class NSimpleBox extends GameNode
 
 	private final Retangulo ret;
 
-	public NSimpleBox(int x, int y, int w, int h, int colorR, int colorG, int colorB, int colorA)
+	private boolean visible = true;
+
+	public NSimpleBox(final int x, final int y, final int w, final int h, final int colorR, final int colorG, final int colorB,
+			final int colorA)
 	{
 		color.setARGB(colorA, colorR, colorG, colorB);
 		ret = new Retangulo(new Ponto(x, y), w, h);
 
 	}
 
-	public NSimpleBox(int x, int y, int w, int h, int colorR, int colorG, int colorB)
+	public NSimpleBox(final int x, final int y, final int w, final int h, final int colorR, final int colorG, final int colorB)
 	{
 		this(x, y, w, h, colorR, colorG, colorB, 255);
 	}
 
 	@Override
-	public void update(long delta)
+	public void update(final long delta)
 	{
 
 	}
 
 	@Override
-	public void render(Canvas canvas)
+	public void render(final Canvas canvas)
 	{
 		canvas.drawRect(ret.toAndroidCanvasRect(), color);
 	}
 
-	public void setColor(int r, int g, int b)
+	public void setColor(final int r, final int g, final int b)
 	{
 		color.setARGB(255, r, g, b);
 	}
 
-	public void setColor(int a, int r, int g, int b)
+	public void setColor(final int a, final int r, final int g, final int b)
 	{
 		color.setARGB(a, r, g, b);
 	}
@@ -56,4 +59,17 @@ public class NSimpleBox extends GameNode
 		final int colorNumber = color.getColor();
 		return Tuple3.from(Color.red(colorNumber), Color.green(colorNumber), Color.blue(colorNumber));
 	}
+
+	@Override
+	public boolean isVisible()
+	{
+		return visible;
+	}
+
+	@Override
+	public void setVisible(final boolean visible)
+	{
+		this.visible = visible;
+	}
+
 }

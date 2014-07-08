c186b03eba3a1bfdb601e19c19783714f2964484
Vinicius Nogueira <viniciusln@gmail.com>
2014-07-08 01:26:35 -0300

    comentário desnecessário

--- HarMegidoAndroid/src/org/vvgaming/harmegido/util/Constantes.java
+++ HarMegidoAndroid/src/org/vvgaming/harmegido/util/Constantes.java
@@ -12,10 +12,8 @@ public class Constantes
 	// varrer o código e fazer uma limpeza dessas inversões entre altura e largura
 	public static final Size camNormalSize = new Size(640, 480);
 	public static final Size camPreviewSize = new Size(160, 120);
-	
-	
-//	public static final Tuple3<Integer, Integer, Integer> ANGEL_COLOR = Tuple3.from(43, 167, 203);
-	public static final Tuple3<Integer, Integer, Integer> ANGEL_COLOR = Tuple3.from(166,148,112);
+
+	public static final Tuple3<Integer, Integer, Integer> ANGEL_COLOR = Tuple3.from(166, 148, 112);
 	public static final Tuple3<Integer, Integer, Integer> DEMON_COLOR = Tuple3.from(255, 0, 0);
 
 }

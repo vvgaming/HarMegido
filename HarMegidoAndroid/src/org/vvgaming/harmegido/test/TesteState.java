919d2ee42a17796a51185d7980ec41d01fc4ad32
Vinicius Nogueira <viniciusln@gmail.com>
2014-07-09 00:17:54 -0300

    teste para mandar grandes arrays de bytes

--- HarMegidoAndroid/src/org/vvgaming/harmegido/test/TesteState.java
+++ HarMegidoAndroid/src/org/vvgaming/harmegido/test/TesteState.java
@@ -1,6 +1,7 @@
 package org.vvgaming.harmegido.test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
@@ -9,6 +10,7 @@ import org.vvgaming.harmegido.lib.model.Enchantment;
 import org.vvgaming.harmegido.lib.model.EnchantmentImage;
 import org.vvgaming.harmegido.lib.model.Match;
 import org.vvgaming.harmegido.lib.model.Match.MatchDuration;
+import org.vvgaming.harmegido.lib.model.OpenCVMatWrapper;
 import org.vvgaming.harmegido.lib.model.Player;
 import org.vvgaming.harmegido.lib.model.Scoreboard;
 import org.vvgaming.harmegido.lib.model.TeamType;
@@ -35,12 +37,12 @@ public class TesteState extends Activity
 	private List<Enchantment> encantamentos = new ArrayList<>();
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState)
+	protected void onCreate(final Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		
+
 		setContentView(R.layout.teste_state);
-		
+
 		final String playerName = "VINICIUS";
 		jogador = Player.from(playerName, DeviceInfo.getDeviceId(), TeamType.DARK);
 		inimigo = Player.from("inimigo", "enemyDevice", TeamType.LIGHT);
@@ -48,19 +50,19 @@ public class TesteState extends Activity
 		((Button) findViewById(R.id.btnListarPartidas)).setOnClickListener(new View.OnClickListener()
 		{
 			@Override
-			public void onClick(View v)
+			public void onClick(final View v)
 			{
 				final Either<Exception, List<String>> retorno = sdf.listarPartidas();
 				setText(formatarRetorno(retorno));
 			}
 		});
-		
+
 		((Button) findViewById(R.id.btnCriarPartida)).setOnClickListener(new View.OnClickListener()
 		{
 			@Override
-			public void onClick(View v)
+			public void onClick(final View v)
 			{
-				Either<Exception, Match> retorno = sdf.criarPartida("partida" + new Random().nextInt(), MatchDuration.FIVE_MINUTES);
+				final Either<Exception, Match> retorno = sdf.criarPartida("partida" + new Random().nextInt(), MatchDuration.FIVE_MINUTES);
 				setText(formatarRetorno(retorno));
 				if (retorno.isRight())
 				{
@@ -68,44 +70,44 @@ public class TesteState extends Activity
 				}
 			}
 		});
-		
+
 		((Button) findViewById(R.id.btnEntrarPartida)).setOnClickListener(new View.OnClickListener()
 		{
 			@Override
-			public void onClick(View v)
+			public void onClick(final View v)
 			{
 				if (partida == null)
 				{
 					setText("Crie uma partida antes de entrar.");
 					return;
 				}
-				Either<Exception, Boolean> retorno = sdf.adicionarJogador(partida.getNomePartida(), jogador);
+				final Either<Exception, Boolean> retorno = sdf.adicionarJogador(partida.getNomePartida(), jogador);
 				setText(formatarRetorno(retorno));
 				sdf.adicionarJogador(partida.getNomePartida(), inimigo);
 			}
 		});
-		
+
 		((Button) findViewById(R.id.btnSairPartida)).setOnClickListener(new View.OnClickListener()
 		{
 			@Override
-			public void onClick(View v)
+			public void onClick(final View v)
 			{
 				if (partida == null)
 				{
 					setText("Crie uma partida antes de sair.");
 					return;
 				}
-				Either<Exception, Boolean> retorno = sdf.removerJogador(partida.getNomePartida(), jogador);
+				final Either<Exception, Boolean> retorno = sdf.removerJogador(partida.getNomePartida(), jogador);
 				setText(formatarRetorno(retorno));
 			}
 		});
-		
+
 		((Button) findViewById(R.id.btnEncontrarPartida)).setOnClickListener(new View.OnClickListener()
 		{
 			@Override
-			public void onClick(View v)
+			public void onClick(final View v)
 			{
-				Either<Exception, Match> retorno = sdf.encontrarPartida(jogador);
+				final Either<Exception, Match> retorno = sdf.encontrarPartida(jogador);
 				if (retorno.isLeft())
 				{
 					setText(formatarRetorno(retorno));
@@ -114,54 +116,61 @@ public class TesteState extends Activity
 				{
 					setText(retorno.getRight().getNomePartida() + "/" + retorno.getRight().getFimPartida());
 				}
-				
+
 			}
 		});
-		
+
 		((Button) findViewById(R.id.btnListarJogadores)).setOnClickListener(new View.OnClickListener()
 		{
 			@Override
-			public void onClick(View v)
+			public void onClick(final View v)
 			{
-				Either<Exception, List<Tuple2<String, List<Tuple2<TeamType, Integer>>>>> retorno = sdf.listarJogadores();
+				final Either<Exception, List<Tuple2<String, List<Tuple2<TeamType, Integer>>>>> retorno = sdf.listarJogadores();
 				setText(formatarRetorno(retorno));
 			}
 		});
-		
+
 		((Button) findViewById(R.id.btnMudarTime)).setOnClickListener(new View.OnClickListener()
 		{
 			@Override
-			public void onClick(View v)
+			public void onClick(final View v)
 			{
-				final TeamType novoTime = jogador.getTime() == TeamType.DARK ? TeamType.LIGHT : TeamType.DARK; 
-				Either<Exception, Boolean> retorno = sdf.mudarTime(partida.getNomePartida(), jogador, novoTime);
+				final TeamType novoTime = jogador.getTime() == TeamType.DARK ? TeamType.LIGHT : TeamType.DARK;
+				final Either<Exception, Boolean> retorno = sdf.mudarTime(partida.getNomePartida(), jogador, novoTime);
 				setText(formatarRetorno(retorno));
 			}
 		});
-		
+
 		((Button) findViewById(R.id.btnEncantar)).setOnClickListener(new View.OnClickListener()
 		{
 			@Override
-			public void onClick(View v)
+			public void onClick(final View v)
 			{
 				if (partida == null)
 				{
 					setText("Crie uma partida antes de encantar.");
 					return;
 				}
-//				EnchantmentImage enchant = EnchantmentImage.from(new byte[100], new byte[200]);
-//				Either<Exception, Boolean> retorno = sdf.encantarObjeto(partida.getNomePartida(), jogador, enchant);
-//				setText(formatarRetorno(retorno));
+				final byte[] bytes = new byte[20000];
+				Arrays.fill(bytes, (byte) -100);
+				final byte[] bytes2 = new byte[20000];
+				Arrays.fill(bytes2, (byte) -100);
+				
+				final EnchantmentImage enchant = EnchantmentImage.from(OpenCVMatWrapper.from(bytes, 10, 10, 0),
+						OpenCVMatWrapper.from(bytes2, 10, 10, 0));
+				final Either<Exception, Boolean> retorno = sdf.encantarObjeto(partida.getNomePartida(), jogador, enchant);
+				setText(formatarRetorno(retorno));
 			}
 		});
-		
+
 		((Button) findViewById(R.id.btnListarEncantamentos)).setOnClickListener(new View.OnClickListener()
 		{
 			@SuppressWarnings("unchecked")
 			@Override
-			public void onClick(View v)
+			public void onClick(final View v)
 			{
-				Either<Exception, Object> retorno = sdf.encontrarPartida(jogador).map(Reflections.lift(Match.class, "getEncantamentos"));
+				final Either<Exception, Object> retorno = sdf.encontrarPartida(jogador).map(
+						Reflections.lift(Match.class, "getEncantamentos"));
 				if (retorno.isRight())
 				{
 					encantamentos = (List<Enchantment>) retorno.getRight();
@@ -169,58 +178,58 @@ public class TesteState extends Activity
 				setText(formatarRetorno(retorno));
 			}
 		});
-		
+
 		((Button) findViewById(R.id.btnDesencantar)).setOnClickListener(new View.OnClickListener()
 		{
 			@Override
-			public void onClick(View v)
+			public void onClick(final View v)
 			{
 				if (partida == null)
 				{
 					setText("Crie uma partida antes de desencantar.");
 					return;
 				}
-				
+
 				if (encantamentos.isEmpty())
 				{
 					setText("Nenhum encantamento para desencantar.");
 					return;
 				}
-				
-				Either<Exception, Boolean> retorno = sdf.desencantarObjeto(partida.getNomePartida(), inimigo, encantamentos.get(0));
+
+				final Either<Exception, Boolean> retorno = sdf.desencantarObjeto(partida.getNomePartida(), inimigo, encantamentos.get(0));
 				setText(formatarRetorno(retorno));
 			}
 		});
-		
+
 		((Button) findViewById(R.id.btnListarDesencamentos)).setOnClickListener(new View.OnClickListener()
 		{
 			@Override
-			public void onClick(View v)
+			public void onClick(final View v)
 			{
 				final StringBuilder sBuilder = new StringBuilder();
-				
-				for (Enchantment enchant : encantamentos)
+
+				for (final Enchantment enchant : encantamentos)
 				{
-					sBuilder.append("[" + enchant.toString() + " / " +  enchant.getDesencantamento().toString() + "]\n");
+					sBuilder.append("[" + enchant.toString() + " / " + enchant.getDesencantamento().toString() + "]\n");
 				}
-				
+
 				setText(sBuilder.toString());
 			}
 		});
-		
+
 		((Button) findViewById(R.id.btnVerPontuacao)).setOnClickListener(new View.OnClickListener()
 		{
 			@Override
-			public void onClick(View v)
+			public void onClick(final View v)
 			{
 				if (partida == null)
 				{
 					setText("Crie uma partida antes de calcular a pontuacao.");
 					return;
 				}
-				
-				Either<Exception, Scoreboard> retorno = sdf.getPontuacao(partida.getNomePartida());
-				
+
+				final Either<Exception, Scoreboard> retorno = sdf.getPontuacao(partida.getNomePartida());
+
 				if (retorno.isLeft())
 				{
 					setText(formatarRetorno(retorno));
@@ -228,7 +237,7 @@ public class TesteState extends Activity
 				else
 				{
 					String theStr = "";
-					for (TeamType time : TeamType.values())
+					for (final TeamType time : TeamType.values())
 					{
 						theStr += time.toString() + ":" + retorno.getRight().getPontuacao(time) + "\n";
 					}
@@ -236,31 +245,31 @@ public class TesteState extends Activity
 				}
 			}
 		});
-		
-		//métodos
-//		sdf.adicionarJogador(nomePartida, jogador) 						//OK
-//		sdf.criarPartida(nomePartida, duracao)   						//OK
-//		sdf.desencantarObjeto(nomePartida, jogador, encantamento)		//OK
-//		sdf.encantarObjeto(nomePartida, jogador, imagem)				//OK
-//		sdf.encontrarPartida(jogador)									//OK
-//		sdf.listarJogadores()											//OK
-//		sdf.listarPartidas()     										//OK
-//		sdf.mudarTime(nomePartida, jogador, novoTime)					//OK
-//		sdf.removerJogador(nomePartida, jogador)						//OK
-//		sdf.getPontuacao(nomePartida)									//OK
-		
+
+		// métodos
+		// sdf.adicionarJogador(nomePartida, jogador) //OK
+		// sdf.criarPartida(nomePartida, duracao) //OK
+		// sdf.desencantarObjeto(nomePartida, jogador, encantamento) //OK
+		// sdf.encantarObjeto(nomePartida, jogador, imagem) //OK
+		// sdf.encontrarPartida(jogador) //OK
+		// sdf.listarJogadores() //OK
+		// sdf.listarPartidas() //OK
+		// sdf.mudarTime(nomePartida, jogador, novoTime) //OK
+		// sdf.removerJogador(nomePartida, jogador) //OK
+		// sdf.getPontuacao(nomePartida) //OK
+
 	}
-	
+
 	private static <A> String formatarRetorno(final Either<Exception, A> either)
 	{
 		return either.isLeft() ? either.getLeft().getMessage() : either.getRight().toString();
 	}
-	
+
 	private final void setText(final String text)
 	{
 		((TextView) findViewById(R.id.txtViewRetorno)).setText(text);
 	}
-	
+
 	@Override
 	protected void onDestroy()
 	{

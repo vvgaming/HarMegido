d6c08dbbff1061d86997f3e86bc37fc360d411e9
Vinicius Nogueira <viniciusln@gmail.com>
2014-07-10 12:14:58 -0300

    apenas um método utilitário

--- HarMegidoAndroid/src/org/vvgaming/harmegido/gameEngine/geometry/Retangulo.java
+++ HarMegidoAndroid/src/org/vvgaming/harmegido/gameEngine/geometry/Retangulo.java
@@ -1,41 +1,50 @@
 package org.vvgaming.harmegido.gameEngine.geometry;
 
 import android.graphics.Rect;
+import android.graphics.RectF;
 
 /**
  * Classe geom�trica que representa o ret�ngulo e suas opera��es
  * 
  * @author Vinicius Nogueira
  */
-public class Retangulo {
+public class Retangulo
+{
 
 	public final Ponto origem;
 	public final float width, height;
 
-	public Retangulo(Ponto origem, float width, float height) {
+	public Retangulo(Ponto origem, float width, float height)
+	{
 		super();
 		this.origem = origem;
 		this.width = width;
 		this.height = height;
 	}
 
-	public Rect toAndroidCanvasRect() {
-		return new Rect((int) origem.x, (int) origem.y, (int) (origem.x + width),
-				(int) (origem.y + height));
+	public Rect toAndroidCanvasRect()
+	{
+		return new Rect((int) origem.x, (int) origem.y, (int) (origem.x + width), (int) (origem.y + height));
 	}
 
-	public boolean contains(Ponto ponto) {
-		return (ponto.x >= origem.x && ponto.x <= (origem.x + width)
-				&& ponto.y >= origem.y && ponto.y <= (origem.y + height));
+	public RectF toAndroidCanvasRectF()
+	{
+		return new RectF(origem.x, origem.y, origem.x + width, origem.y + height);
 	}
 
-	public Ponto getCenter() {
+	public boolean contains(Ponto ponto)
+	{
+		return (ponto.x >= origem.x && ponto.x <= (origem.x + width) && ponto.y >= origem.y && ponto.y <= (origem.y + height));
+	}
+
+	public Ponto getCenter()
+	{
 		return new Ponto(origem.x + width / 2, origem.y + height / 2);
 	}
 
-	public static Retangulo fromCenter(Ponto centro, float w, float h) {
-		return new Retangulo(new Ponto(centro.x - w / 2, centro.y - h / 2), w,
-				h);
+	public static Retangulo fromCenter(Ponto centro, float w, float h)
+	{
+		return new Retangulo(new Ponto(centro.x - w / 2, centro.y - h / 2), w, h);
 	}
 
 }

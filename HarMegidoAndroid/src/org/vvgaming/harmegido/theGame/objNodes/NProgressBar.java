6aa72bb43ed65d498ee3ffbf9239b948a412e57d
Vinicius Nogueira <viniciusln@gmail.com>
2014-07-13 03:50:07 -0300

    funcionalidade extra para inverter uma progress bar para vertical tb

--- HarMegidoAndroid/src/org/vvgaming/harmegido/theGame/objNodes/NProgressBar.java
+++ HarMegidoAndroid/src/org/vvgaming/harmegido/theGame/objNodes/NProgressBar.java
@@ -14,26 +14,30 @@ public class NProgressBar extends GameNode
 	private final Retangulo ret;
 	private Retangulo retToDraw;
 
+	private boolean larguraProgress = true;
+	private boolean alturaProgress = false;
+
 	private float progress = 0f;
 
-	public NProgressBar(float x, float y, float w, float h)
+	public NProgressBar(final float x, final float y, final float w, final float h)
 	{
 		ret = new Retangulo(new Ponto(x, y), w, h);
 	}
 
 	@Override
-	public void update(long delta)
+	public void update(final long delta)
 	{
-		retToDraw = new Retangulo(ret.origem, ret.width * progress, ret.height);
+		retToDraw = new Retangulo(ret.origem, larguraProgress ? ret.width * progress : ret.width, alturaProgress ? ret.height * progress
+				: ret.height);
 	}
 
 	@Override
-	public void render(Canvas canvas)
+	public void render(final Canvas canvas)
 	{
 		canvas.drawRect(retToDraw.toAndroidCanvasRect(), color);
 	}
 
-	public void setProgress(float percentProgress)
+	public void setProgress(final float percentProgress)
 	{
 		progress = percentProgress;
 		if (progress > 1)
@@ -59,6 +63,41 @@ public class NProgressBar extends GameNode
 	public boolean isFull()
 	{
 		return progress >= 1;
+	}
+
+	public boolean isLarguraProgress()
+	{
+		return larguraProgress;
+	}
+
+	/**
+	 * indicador se o a barra de progresso cresce em largura (por padrão sim!)
+	 * 
+	 * @param larguraProgress
+	 */
+	public void setLarguraProgress(final boolean larguraProgress)
+	{
+		this.larguraProgress = larguraProgress;
+	}
+
+	public boolean isAlturaProgress()
+	{
+		return alturaProgress;
+	}
+
+	/**
+	 * indicador se o a barra de progresso cresce em altura
+	 * 
+	 * @param alturaProgress
+	 */
+	public void setAlturaProgress(final boolean alturaProgress)
+	{
+		this.alturaProgress = alturaProgress;
+	}
+
+	public void setARGB(final int a, final int r, final int g, final int b)
+	{
+		color.setARGB(a, r, g, b);
 	}
 
 }
